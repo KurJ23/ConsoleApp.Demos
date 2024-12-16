6b513d545dcbcd83fb33530c0e059eaeb37ae7d7
KurJ23 <kurtjansen0123@gmail.com>
2024-12-16 12:56:51 +0200

    Add project files.

---
+++ ConsoleApp.OutputDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! My name is Kurt Jansen");
+Console.WriteLine("I am from SA");
+Console.WriteLine("Anything you want to say");
+
+Console.Write("Line 1");
+Console.Write("Line 2");
+
+
---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string name = "Kurt Jansen";
+
+Console.WriteLine(name);
+Console.WriteLine("I am " +  name);
+Console.WriteLine($"They call me {name}");
+Console.WriteLine("I was given the name {0}", name);
+
+int age = 54;
+int retirementYearsLeft = 11;
+int retirementAge = age + retirementYearsLeft;
+
+Console.WriteLine("my age is: " + age);
+Console.WriteLine("my retirementAge is: " + retirementAge);
+
+bool isRetired = false;
+Console.WriteLine("Am I retired? " + isRetired);
